deab60002e1f5da8b469730e73525579931d4e1d
Will-Wrightwa <wawwright@gmail.com>
2017-05-09 22:50:56 -0500

    changed file structure

---
+++ app/components/app.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+require('../styles/index.css')
+const App = (props)=>{
+  return (
+    <div>
+      {/*add Components here...*/}
+      <h1 className="test">foo</h1>
+    </div>
+  )
+}
+
+module.exports = App;
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const App = require('./app');
+const App = require('./components/app');
 
 
 ReactDOM.render(<App />,document.getElementById('app'))
