425350cb2f71267ffcd526a292a1b974e26d1c37
Will-Wrightwa <wawwright@gmail.com>
2017-05-09 22:35:37 -0500

    basic react webpack boilerplate

---
+++ app/app.js
@@ -0,0 +1,16 @@
+const React = require('react');
+//require components here...
+//const MyComponent = require('./my_component')
+require('./index.css')
+const App = (props)=>{
+  return (
+    <div>
+      {/*add Components here...*/}
+    </div>
+  )
+}
+
+
+
+
+module.exports = App;
